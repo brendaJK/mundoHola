9c96129e4022111ba5e4ce9a26c33f21769e3025
brendaJK <es.bren.27.gtz.p@gmail.com>
2023-02-23 21:14:16 -0600

    Esta es mi primer version

---
+++ src/javaapplication8/JavaApplication8.java
@@ -0,0 +1,16 @@
+
+package javaapplication8;
+
+/**
+ *
+ * @author esbre
+ */
+public class JavaApplication8 {
+
+    
+    public static void main(String[] args) {
+        
+        System.out.println("Hola Mundo");
+    }
+    
+}
